225a523b2b0f278f45aa8183237fd7eee1136d9e
Minh Le <202490311+leminh646@users.noreply.github.com>
2025-07-11 11:06:08 -0700

    quick sort algorithm

---
+++ quickSort.py
@@ -0,0 +1,21 @@
+def partition(array, low, high):
+
+    # choose the rightmost element as pivot
+    pivot = array[high]
+
+    # pointer for greater element
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
+
+
+def quickSort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quickSort(array, low, pi - 1)
+        quickSort(array, pi + 1, high)
